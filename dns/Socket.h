7aeef78922cb607828464bdef0b642d0b8d7b1aa
fabian <fabian.terhorst@gmail.com>
2019-11-14 11:20:12 +0100

    add getaddresses

---
+++ dns/Socket.h
@@ -0,0 +1,66 @@
+//
+// Created by Fabian Terhorst on 17.10.19.
+//
+
+#ifndef DAYTIME_CLIENT_SOCKET_H
+#define DAYTIME_CLIENT_SOCKET_H
+
+// For socket api
+#include <sys/socket.h>
+// For protocol defines
+#include <netinet/in.h>
+// For perror
+#include <stdio.h>
+// For close
+#include <unistd.h>
+#include <pthread.h>
+// For addrinfo
+#include <netdb.h>
+// For inet_ntop
+#include <arpa/inet.h>
+
+int Socket(int address_family, int type, int protocol);
+
+int Bind(int fd, const struct sockaddr* socket_address, size_t socket_address_size);
+
+ssize_t Recvfrom(int fd, void* buffer, size_t buffer_size, int flags, struct sockaddr* client_socket_address,
+                 socklen_t* client_socket_address_size);
+
+void Sendto(int fd, const void* buffer, size_t buffer_size, int flags, const struct sockaddr* client_socket_address,
+            socklen_t client_socket_address_size);
+
+void Select(int nfds, fd_set* read_fds, fd_set* write_fds, fd_set* except_fds, struct timeval* timeout);
+
+ssize_t Read(int fd, void* buffer, size_t buffer_size);
+
+ssize_t Recv(int fd, void* buffer, size_t buffer_size, int flags);
+
+ssize_t Send(int fd, const void* buffer, size_t buffer_size, int flags);
+
+void Connect(int fd, struct sockaddr* server_socket_address, size_t server_socket_address_size);
+
+void Listen(int fd, int backlog);
+
+int Accept(int fd, struct sockaddr* client_socket_address, socklen_t* client_socket_address_size);
+
+void Close(int fd);
+
+void Shutdown(int fd, int how);
+
+void Pthread_create(pthread_t* p_thread,
+                    const pthread_attr_t* attr,
+                    void* (*callback)(void*),
+                    void* user_data);
+
+void Pthread_attr_init(pthread_attr_t* attr);
+
+void Pthread_attr_setdetachstate(pthread_attr_t* attr, int state);
+
+int Getaddrinfo(const char * host_name, const char * service_name,
+                const struct addrinfo * hints,
+                struct addrinfo ** result);
+
+const char *Inet_ntop(int af, const void *src,
+                      char *dst, socklen_t size);
+
+#endif //DAYTIME_CLIENT_SOCKET_H
---
+++ dns/getaddresses.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include "Socket.h"
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+void fill_hints(struct addrinfo* hints) {
+    memset(hints, 0, sizeof(struct addrinfo));
+    hints->ai_family = AF_UNSPEC;
+    hints->ai_socktype = 0;
+    hints->ai_protocol = 0;
+    hints->ai_flags = 0;
+    hints->ai_canonname = NULL;
+    hints->ai_addr = NULL;
+    hints->ai_next = NULL;
+}
+
+int main(int argc, char** argv) {
+
+    struct addrinfo hints;
+
+    struct addrinfo* result, * curr;
+
+    char host_name_buffer[NI_MAXHOST];
+
+    if (argc < 2) {
+        fprintf(stderr, "Provide address\n");
+        return 0;
+    }
+
+    printf("IP addresses for: %s\n\n", argv[1]);
+
+    fill_hints(&hints);
+
+    Getaddrinfo(argv[1], NULL, &hints, &result);
+
+    curr = result;
+
+    do {
+        if (curr->ai_addr->sa_family == AF_INET) {
+            Inet_ntop(curr->ai_addr->sa_family, &(((struct sockaddr_in*) curr->ai_addr)->sin_addr),
+                      host_name_buffer, sizeof(host_name_buffer));
+        } else if (curr->ai_addr->sa_family == AF_INET6) {
+            Inet_ntop(curr->ai_addr->sa_family, &(((struct sockaddr_in6*) curr->ai_addr)->sin6_addr),
+                      host_name_buffer, sizeof(host_name_buffer));
+        }
+
+        printf("Inet_ntop: %s\n", host_name_buffer);
+
+        getnameinfo(curr->ai_addr, curr->ai_addr->sa_len, host_name_buffer, sizeof(host_name_buffer), NULL, 0,
+                    NI_NUMERICHOST);
+        printf("getnameinfo: %s\n", host_name_buffer);
+
+        printf("------\n");
+
+    } while ((curr = curr->ai_next) != NULL);
+
+    freeaddrinfo(result);
+    return 0;
+}
