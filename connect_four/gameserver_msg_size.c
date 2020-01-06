5e19de482f7dac32db0f8e8edd5c1ca211c415ed
fabian <fabian.terhorst@gmail.com>
2020-01-06 16:38:10 +0100

    Update gameserver_msg_size.c

--- connect_four/gameserver_msg_size.c
+++ connect_four/gameserver_msg_size.c
@@ -7,9 +7,5 @@ void init_min_msg_size(uint16_t* msg_min_size) {
     msg_min_size[MSG_REG_NACK] = (uint16_t) sizeof(struct msg_header_t);
     msg_min_size[MSG_PEER_INFO] = (uint16_t) sizeof(msg_peer_info);
     msg_min_size[MSG_PEER_INFO_ACK] = (uint16_t) sizeof(struct msg_header_t);
-    msg_min_size[MSG_MOVE] = (uint16_t) sizeof(msg_move);
-    msg_min_size[MSG_MOVE_ACK] = (uint16_t) sizeof(msg_move_ack);
-    msg_min_size[MSG_HEARTBEAT] = (uint16_t) sizeof(struct msg_header_t);
-    msg_min_size[MSG_HEARTBEAT_ACK] = (uint16_t) sizeof(struct msg_header_t);
     msg_min_size[MSG_UNKNOWN_MSG_TYPE] = (uint16_t) sizeof(struct msg_header_t);
 }
